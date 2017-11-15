ef83c8b424e745bb4305b4bf18c02453241c0f3b
Star <576681253@qq.com>
2017-11-15 16:06:30 +0800

    Initial Commit 2

---
+++ WexSDKHelper.h
@@ -0,0 +1,12 @@
+//
+//  WexSDKHelper.h
+//  Kiwi
+//
+//  Created by 星星 on 2017/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WexSDKHelper : NSObject
+
+@end
---
+++ WexSDKHelper.m
@@ -0,0 +1,12 @@
+//
+//  WexSDKHelper.m
+//  Kiwi
+//
+//  Created by 星星 on 2017/11/15.
+//
+
+#import "WexSDKHelper.h"
+
+@implementation WexSDKHelper
+
+@end
